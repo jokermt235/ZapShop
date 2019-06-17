3802722ecec8bfb4012a8b272be39e1d1de26ac3
mutalip <jokermt2357@gmail.com>
2019-06-17 19:22:45 +0600

    ZapShop android

---
+++ app/src/main/java/devel/exesoft/com/zapshop/MainActivity.java
@@ -0,0 +1,24 @@
+package devel.exesoft.com.zapshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        toolbar.setTitle(getString(R.string.main_toolbar_title));
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
