1901cdb7c71ba5ff8db05c35f75dbf0c3a47498c
jokermt235 <jokermt235@yandex.ru>
2019-07-05 01:26:37 +0600

    Menu item with icons and slide on click

--- app/src/main/java/devel/exesoft/com/zapshop/MainActivity.java
+++ app/src/main/java/devel/exesoft/com/zapshop/MainActivity.java
@@ -2,16 +2,23 @@ package devel.exesoft.com.zapshop;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import devel.exesoft.com.zapshop.controlles.CategoryController;
 import devel.exesoft.com.zapshop.databinding.ActivityMainBinding;
 import devel.exesoft.com.zapshop.view_model.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivty";
 
     public MainViewModel mainViewModel;
 
@@ -23,10 +30,12 @@ public class MainActivity extends AppCompatActivity {
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         mainViewModel = new MainViewModel(this);
         activityMainBinding.setViewModel(mainViewModel);
+        initDrawerMenu();
         activityMainBinding.executePendingBindings();
         activityMainBinding.mainToolbar.setTitle(getString(R.string.main_toolbar_title));
         activityMainBinding.mainToolbar.setNavigationIcon(R.drawable.ic_menu_black_24dp);
         setSupportActionBar(activityMainBinding.mainToolbar);
+        initNavogationAction();
         CategoryController.getCategories(this);
     }
 
@@ -36,6 +45,14 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    public void initDrawerMenu(){
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, activityMainBinding.drawerLayout, activityMainBinding.mainToolbar, R.string.app_name, R.string.app_name);
+
+        //drawer.setDrawerListener(toggle);
+        activityMainBinding.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -44,5 +61,14 @@ public class MainActivity extends AppCompatActivity {
             case R.id.main_toolbar_cart: mainViewModel.onCartItemClicked();break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void initNavogationAction(){
+        activityMainBinding.mainToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activityMainBinding.drawerLayout.openDrawer(Gravity.START);
+            }
+        });
     }
 }
