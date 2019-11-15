b983096ec9b73c03d0b7eaecc6b25a1fc35604c9
mutalip <jokermt2357@gmail.com>
2019-11-15 18:32:35 +0600

    Adding images to banner

--- app/src/main/java/devel/exesoft/com/zapshop/adapters/BannerViewAdapter.java
+++ app/src/main/java/devel/exesoft/com/zapshop/adapters/BannerViewAdapter.java
@@ -4,49 +4,36 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.smarteist.autoimageslider.SliderViewAdapter;
 
+import java.util.ArrayList;
+
 import devel.exesoft.com.zapshop.R;
+import devel.exesoft.com.zapshop.models.Banner;
 
 public class BannerViewAdapter extends SliderViewAdapter<BannerViewAdapter.SliderAdapterVH> {
     private Context context;
+    public ArrayList<Banner> items;
     @Override
     public SliderAdapterVH onCreateViewHolder(ViewGroup parent) {
         View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.banner_item, null);
         return new SliderAdapterVH(inflate);
     }
 
+    public void setItems(ArrayList<Banner> items){
+        this.items = items;
 
+    }
 
     @Override
     public void onBindViewHolder(SliderAdapterVH viewHolder, int position) {
-        /*viewHolder.textViewDescription.setText("This is slider item " + position);
-
-        switch (position) {
-            case 0:
-                Glide.with(viewHolder.itemView)
-                        .load("https://images.pexels.com/photos/218983/pexels-photo-218983.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
-                        .into(viewHolder.imageViewBackground);
-                break;
-            case 1:
-                Glide.with(viewHolder.itemView)
-                        .load("https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260")
-                        .into(viewHolder.imageViewBackground);
-                break;
-            case 2:
-                Glide.with(viewHolder.itemView)
-                        .load("https://images.pexels.com/photos/929778/pexels-photo-929778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
-                        .into(viewHolder.imageViewBackground);
-                break;
-            default:
-                Glide.with(viewHolder.itemView)
-                        .load("https://images.pexels.com/photos/218983/pexels-photo-218983.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
-                        .into(viewHolder.imageViewBackground);
-                break;
-
-        }*/
-
+        viewHolder.textViewDescription.setText("This is slider item " + position);
+        if(items.size() > 0){
+            viewHolder.imageViewBackground.setImageBitmap(items.get(position).getImage());
+        }
     }
 
     @Override
@@ -56,15 +43,15 @@ public class BannerViewAdapter extends SliderViewAdapter<BannerViewAdapter.Slide
 
     class SliderAdapterVH extends SliderViewAdapter.ViewHolder {
 
-        /*View itemView;
+        View itemView;
         ImageView imageViewBackground;
-        TextView textViewDescription;*/
+        TextView textViewDescription;
 
         public SliderAdapterVH(View itemView) {
             super(itemView);
-            /*imageViewBackground = itemView.findViewById(R.id.iv_auto_image_slider);
-            textViewDescription = itemView.findViewById(R.id.tv_auto_image_slider);*/
-            //this.itemView = itemView;
+            imageViewBackground = itemView.findViewById(R.id.ml_banner_image);
+            textViewDescription = itemView.findViewById(R.id.ml_banner_text);
+            this.itemView = itemView;
         }
     }
 }
